9f9b774a7238af310b1bc6fb2e470e681f24311e
Dark Angel <jonathanyombo@gmail.com>
2023-04-01 13:28:52 +0200

    Renamed menu.rs -> app.rs

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,10 @@
-use prettytable::{row, table, Table, format};
+use prettytable::{row, table, format};
 use crate::repertoire::{Contact, deserialize_contacts, serialize_contacts};
 
 // menu function to display the menu and get the user's choice
-fn menu() {
+pub fn menu() {
     loop {
-        println!("Menu : 1. Add a contact, 2. Display all contacts, 3. Search a contact, 4. Delete a contact, 5. Quit");
+        println!("Menu : 1. Add a contact, 2. Display all contacts, 3. Search a contact, 4. Delete a contact, 0. Quit");
         let mut choice = String::new();
         std::io::stdin().read_line(&mut choice).expect("Failed to read line");
         let choice: u8 = choice.trim().parse().expect("Please type a number!");
@@ -14,11 +14,11 @@ fn menu() {
             2 => display_contacts(),
             3 => search_contact(),
             4 => delete_contact(),
-            5 => {
+            0 => {
                 println!("Goodbye!");
                 break;
             }
-            _ => println!("Please choose a number between 1 and 5"),
+            _ => println!("Please choose a number within the menu!"),
         }
     }
 
