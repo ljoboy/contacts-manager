66fa646a9809ffd1ced95ac3a2ffd2b153bd3637
Dark Angel <jonathanyombo@gmail.com>
2023-04-01 13:30:05 +0200

    Contacts file constant

--- src/repertoire.rs
+++ src/repertoire.rs
@@ -3,6 +3,8 @@ use serde::{Serialize, Deserialize};
 use std::fs::File;
 use std::io::{Write, Read};
 
+const CONTACTS_FILE: &str = "contacts.json";
+
 #[derive(Serialize, Deserialize)]
 pub struct Contact {
     phone: String,
@@ -30,18 +32,6 @@ impl Contact {
     pub fn get_address(&self) -> &String {
         &self.address
     }
-
-    pub fn set_phone(&mut self, phone: String) {
-        self.phone = phone;
-    }
-
-    pub fn set_name(&mut self, name: String) {
-        self.name = name;
-    }
-
-    pub fn set_address(&mut self, address: String) {
-        self.address = address;
-    }
 }
 
 impl Display for Contact {
@@ -52,7 +42,7 @@ impl Display for Contact {
 
 // serialize a vector of contacts to a file
 pub fn serialize_contacts(contacts: &Vec<Contact>) -> std::io::Result<()> {
-    let mut file = File::create("contacts.json")?;
+    let mut file = File::create(CONTACTS_FILE)?;
     let serialized = serde_json::to_string(&contacts)?;
     file.write_all(serialized.as_bytes())?;
     Ok(())
@@ -60,7 +50,7 @@ pub fn serialize_contacts(contacts: &Vec<Contact>) -> std::io::Result<()> {
 
 // deserialize a vector of contacts from a file
 pub fn deserialize_contacts() -> std::io::Result<Vec<Contact>> {
-    let mut file = File::open("contacts.json")?;
+    let mut file = File::open(CONTACTS_FILE)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     let contacts: Vec<Contact> = serde_json::from_str(&contents)?;
