1752590d511450cb976fe26043b003890157772c
Dark Angel <jonathanyombo@gmail.com>
2023-04-01 11:49:25 +0200

    read and write contacts from file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::fmt::{Display, Formatter};
+use serde::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::{Write, Read};
 
+#[derive(Serialize, Deserialize)]
 struct Contact {
     phone: String,
     first_name: String,
@@ -22,7 +26,27 @@ impl Display for Contact {
     }
 }
 
-fn main() {
-    let contact = Contact::new("123456789".to_string(), "John".to_string(), "Doe".to_string());
-    println!("{}", contact);
+// serialize a vector of contacts to a file
+fn serialize_contacts(contacts: &Vec<Contact>) -> std::io::Result<()> {
+    let mut file = File::create("contacts.json")?;
+    let serialized = serde_json::to_string(&contacts)?;
+    file.write_all(serialized.as_bytes())?;
+    Ok(())
+}
+
+// deserialize a vector of contacts from a file
+fn deserialize_contacts() -> std::io::Result<Vec<Contact>> {
+    let mut file = File::open("contacts.json")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let contacts: Vec<Contact> = serde_json::from_str(&contents)?;
+    Ok(contacts)
+}
+
+fn main() -> std::io::Result<()> {
+    let contacts = vec![
+        Contact::new("123456789".to_string(), "John".to_string(), "Doe".to_string()),
+        Contact::new("1234567890".to_string(), "John".to_string(), "Doe".to_string()),
+    ];
+    Ok(())
 }
